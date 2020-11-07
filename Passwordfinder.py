ce083a736c432068bfe047bbe6b0f9c5df6a0d3c
Berke Udunman <berkeudunman41@gmail.com>
2020-11-07 20:44:42 +0000

    passwordfinder

--- Passwordfinder.py
+++ Passwordfinder.py
@@ -7,8 +7,7 @@ password=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
 guess=""
 
 while user_pass != guess :
-  guess=""
   for letter in range(len(user_pass)):
-    guess_letter=password[randint(0,25)]
-    guess=str(guess_letter)+ str(guess)
+    guess_letter=password[randint(0,27)]
+    guess=str(guess_letter)+ guess
   print(guess)
